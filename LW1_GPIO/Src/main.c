211cddb6b3b7f8a8b82e49070648bcd19b9eee4f
HixtyGamer <42943865+HixtyGamer@users.noreply.github.com>
2021-11-01 12:09:47 +0300

    Выполнил творческое задание 4, а также изменил типы некоторых переменных во всех заданиях
    Лабораторная работа 1

--- LW1_GPIO/Src/main.c
+++ LW1_GPIO/Src/main.c
@@ -8,6 +8,7 @@ void cr_task1();
 void cr_task2();
 void cr_task3();
 void cr_task4();
+void cr_taskA();
 
 int main(void)
 {
@@ -17,7 +18,8 @@ int main(void)
 	//cr_task1();
 	//cr_task2();
 	//cr_task3();
-	cr_task4();
+	//cr_task4();
+	cr_taskA();
 }
 
 void task_1()
@@ -85,7 +87,7 @@ void task_3()
 	GPIOB->MODER &= ~(GPIO_MODER_MODE13 | GPIO_MODER_MODE15);
 
 	uint8_t press_count = 0;
-	uint8_t is_pressed = 0;
+	_Bool is_pressed = 0;
 
 	while(1)
 	{
@@ -143,27 +145,27 @@ void cr_task1()
 	GPIOB->MODER &= ~(GPIO_MODER_MODE12);
 
 	uint8_t press_count = 0;
-	uint8_t is_pressed = 0;
+	_Bool is_pressed = 0;
 	//счётчик кадров анимации
 	uint8_t frame = 0;
 
 	//кадры анимации "победы"
-	uint8_t victory_frames[][4] = { { 0, 0, 0, 1 },
-									 { 0, 0, 1, 0 },
-									 { 0, 1, 0, 0 },
-									 { 1, 0, 0, 0 },
-									 { 0, 1, 0, 0 },
-									 { 0, 0, 1, 0 } };
+	_Bool victory_frames[][4] = { { 0, 0, 0, 1 },
+								  { 0, 0, 1, 0 },
+								  { 0, 1, 0, 0 },
+								  { 1, 0, 0, 0 },
+								  { 0, 1, 0, 0 },
+								  { 0, 0, 1, 0 } };
 
 	//кадры анимации "ошибки"
-	uint8_t error_frames[][4] = { { 0, 0, 0, 0 },
-								   { 1, 0, 0, 0 },
-								   { 1, 1, 0, 0 },
-								   { 1, 1, 1, 0 },
-								   { 1, 1, 1, 1 },
-								   { 0, 1, 1, 1 },
-								   { 0, 0, 1, 1 },
-								   { 0, 0, 0, 1 } };
+	_Bool error_frames[][4] = { { 0, 0, 0, 0 },
+								{ 1, 0, 0, 0 },
+								{ 1, 1, 0, 0 },
+								{ 1, 1, 1, 0 },
+								{ 1, 1, 1, 1 },
+								{ 0, 1, 1, 1 },
+								{ 0, 0, 1, 1 },
+								{ 0, 0, 0, 1 } };
 
 	//количество кадров анимации "победы"
 	uint8_t victory_num_of_frames = sizeof(victory_frames) / sizeof(victory_frames[0]);
@@ -348,9 +350,9 @@ void cr_task4()
 	GPIOB->MODER &= ~(GPIO_MODER_MODE12);
 
 	uint8_t counter = 0,
-			 buffer = 0,
-			 is_pressed = 0,
-			 b1, b2, b3, b4;
+			buffer = 0;
+	_Bool is_pressed = 0,
+		  b1, b2, b3, b4;
 
 	while(1)
 	{
@@ -402,6 +404,100 @@ void cr_task4()
 		if((GPIOB->IDR & GPIO_IDR_ID12) == 0)
 			is_pressed = 1;
 		else
+			is_pressed = 0;
+	}
+}
+
+void cr_taskA()
+{
+	/*Я так до конца не разобрался где лучше использовать ODR, а где BSRR. В ODR можно напрямую менять состояния выводов,
+     * когда как в BSRR для установки и сброса нужно выставлять единицы в отдельные части регистра.
+	 * То, как я использовал ODR в творческом задании 1, очень похоже на то, как нужно работать с BSRR,
+	 * поэтому на примере данного задания большой разницы между ними я не заметил.
+	 * Разве что очищать состояния выводов удобнее и понятнее через BSRR. */
+	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOEEN | RCC_AHB2ENR_GPIOBEN;
+
+	GPIOE->MODER &= ~(GPIO_MODER_MODE12
+					| GPIO_MODER_MODE13
+					| GPIO_MODER_MODE14
+					| GPIO_MODER_MODE15);
+	GPIOE->MODER |= 1 << GPIO_MODER_MODE12_Pos
+				  | 1 << GPIO_MODER_MODE13_Pos
+				  | 1 << GPIO_MODER_MODE14_Pos
+				  | 1 << GPIO_MODER_MODE15_Pos;
+
+	GPIOB->MODER &= ~(GPIO_MODER_MODE12);
+
+	uint8_t press_count = 0;
+	_Bool is_pressed = 0;
+	//счётчик кадров анимации
+	uint8_t frame = 0;
+
+	//кадры анимации "победы"
+	_Bool victory_frames[][4] = { { 0, 0, 0, 1 },
+								  { 0, 0, 1, 0 },
+								  { 0, 1, 0, 0 },
+								  { 1, 0, 0, 0 },
+								  { 0, 1, 0, 0 },
+								  { 0, 0, 1, 0 } };
+
+	//кадры анимации "ошибки"
+	_Bool error_frames[][4] = { { 0, 0, 0, 0 },
+								{ 1, 0, 0, 0 },
+								{ 1, 1, 0, 0 },
+								{ 1, 1, 1, 0 },
+								{ 1, 1, 1, 1 },
+								{ 0, 1, 1, 1 },
+								{ 0, 0, 1, 1 },
+								{ 0, 0, 0, 1 } };
+
+	//количество кадров анимации "победы"
+	uint8_t victory_num_of_frames = sizeof(victory_frames) / sizeof(victory_frames[0]);
+	//количество кадров анимации "ошибки"
+	uint8_t error_num_of_frames = sizeof(error_frames) / sizeof(error_frames[0]);
+
+	while(1)
+	{
+		dumb_delay(100000);
+
+		//очистка состояния загораемых светодиодов
+		GPIOE->BSRR |= GPIO_BSRR_BR12
+					 | GPIO_BSRR_BR13
+					 | GPIO_BSRR_BR14
+					 | GPIO_BSRR_BR15;
+
+		//изменение анимации после нажатия кнопки
+		if(press_count == 0)
+		{
+			if(frame >= victory_num_of_frames)
+				frame = 0;
+
+			GPIOE->BSRR |= victory_frames[frame][0] << GPIO_BSRR_BS12_Pos
+						 | victory_frames[frame][1] << GPIO_BSRR_BS13_Pos
+						 | victory_frames[frame][2] << GPIO_BSRR_BS14_Pos
+						 | victory_frames[frame][3] << GPIO_BSRR_BS15_Pos;
+		}
+		else
+		{
+			if(frame >= error_num_of_frames)
+				frame = 0;
+
+			GPIOE->BSRR |= error_frames[frame][0] << GPIO_BSRR_BS12_Pos
+						 | error_frames[frame][1] << GPIO_BSRR_BS13_Pos
+						 | error_frames[frame][2] << GPIO_BSRR_BS14_Pos
+						 | error_frames[frame][3] << GPIO_BSRR_BS15_Pos;
+		}
+
+		frame++;
+
+		if((GPIOB->IDR & GPIO_IDR_ID12) == 0 && is_pressed == 0)
+		{
+			is_pressed = 1;
+			//изменение значения счётчика нажатий с 0 на 1 или с 1 на 0
+			press_count ^= 1;
+			frame = 0;
+		}
+		else if(GPIOB->IDR & GPIO_IDR_ID12)
 			is_pressed = 0;
 	}
 }
