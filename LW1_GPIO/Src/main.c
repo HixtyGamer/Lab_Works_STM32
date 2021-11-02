aa638d1a82d22d6acdeec1bab0ff244386b1eec7
HixtyGamer <42943865+HixtyGamer@users.noreply.github.com>
2021-11-02 19:57:24 +0300

    Исправил недочёты в заданиях 1-3 и творческих заданиях 1-4 и A
    Лабораторная работа 1

--- LW1_GPIO/Src/main.c
+++ LW1_GPIO/Src/main.c
@@ -26,8 +26,8 @@ void task_1()
 {
 	RCC->AHB2ENR |= RCC_AHB2ENR_GPIODEN | RCC_AHB2ENR_GPIOEEN;
 
-	GPIOD->MODER &= ~(GPIO_MODER_MODE2_Msk);
-	GPIOD->MODER |= 1 << GPIO_MODER_MODE2_Pos;
+	GPIOD->MODER &= ~(GPIO_MODER_MODE9_Msk);
+	GPIOD->MODER |= 1 << GPIO_MODER_MODE9_Pos;
 
 	GPIOE->MODER &= ~(GPIO_MODER_MODE7_Msk);
 	GPIOE->MODER |= 1 << GPIO_MODER_MODE7_Pos;
@@ -60,14 +60,22 @@ void task_2()
 		dumb_delay(10000);
 
 		if((GPIOB->IDR & GPIO_IDR_ID12) == 0)
-			GPIOE->ODR = 1 << GPIO_ODR_OD2_Pos;
+		{
+			GPIOE->BSRR = GPIO_BSRR_BS2;
+		}
 		else
-			GPIOE->ODR = 0 << GPIO_ODR_OD2_Pos;
+		{
+			GPIOE->BSRR = GPIO_BSRR_BR2;
+		}
 
 		if((GPIOB->IDR & GPIO_IDR_ID14) == 0)
-			GPIOD->ODR = 1 << GPIO_ODR_OD2_Pos;
+		{
+			GPIOD->BSRR = GPIO_BSRR_BS2;
+		}
 		else
-			GPIOD->ODR = 0 << GPIO_ODR_OD2_Pos;
+		{
+			GPIOD->BSRR = GPIO_BSRR_BR2;
+		}
 	}
 }
 
@@ -75,19 +83,19 @@ void task_3()
 {
 	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOEEN | RCC_AHB2ENR_GPIOBEN;
 
-	GPIOE->MODER &= ~(GPIO_MODER_MODE13
+	GPIOE->MODER &= ~(GPIO_MODER_MODE12
+					| GPIO_MODER_MODE13
 					| GPIO_MODER_MODE14
-					| GPIO_MODER_MODE15
-					| GPIO_MODER_MODE0);
-	GPIOE->MODER |= 1 << GPIO_MODER_MODE13_Pos
+					| GPIO_MODER_MODE15);
+	GPIOE->MODER |= 1 << GPIO_MODER_MODE12_Pos
+				  | 1 << GPIO_MODER_MODE13_Pos
 				  | 1 << GPIO_MODER_MODE14_Pos
-				  | 1 << GPIO_MODER_MODE15_Pos
-				  | 1 << GPIO_MODER_MODE0_Pos;
+				  | 1 << GPIO_MODER_MODE15_Pos;
 
 	GPIOB->MODER &= ~(GPIO_MODER_MODE13 | GPIO_MODER_MODE15);
 
 	uint8_t press_count = 0;
-	_Bool is_pressed = 0;
+	uint8_t is_pressed = 0;
 
 	while(1)
 	{
@@ -95,34 +103,22 @@ void task_3()
 
 		if((GPIOB->IDR & GPIO_IDR_ID13) == 0 && (press_count < 4) && is_pressed == 0)
 		{
-			switch(press_count)
-			{
-			case 0:
-				GPIOE->ODR |= 1 << GPIO_ODR_OD13_Pos;
-				break;
-			case 1:
-				GPIOE->ODR |= 1 << GPIO_ODR_OD14_Pos;
-				break;
-			case 2:
-				GPIOE->ODR |= 1 << GPIO_ODR_OD15_Pos;
-				break;
-			case 3:
-				GPIOE->ODR |= 1 << GPIO_ODR_OD0_Pos;
-				break;
-			}
+			GPIOE->ODR |= 1 << (GPIO_ODR_OD12_Pos + press_count);
 
 			is_pressed = 1;
 			press_count += 1;
 		}
 		else if(GPIOB->IDR & GPIO_IDR_ID13)
+		{
 			is_pressed = 0;
+		}
 
 		if((GPIOB->IDR & GPIO_IDR_ID15) == 0)
 		{
-			GPIOE->ODR &= ~(GPIO_ODR_OD13
+			GPIOE->ODR &= ~(GPIO_ODR_OD12
+						  | GPIO_ODR_OD13
 						  | GPIO_ODR_OD14
-						  | GPIO_ODR_OD15
-						  | GPIO_ODR_OD0);
+						  | GPIO_ODR_OD15);
 
 			press_count = 0;
 		}
@@ -145,27 +141,27 @@ void cr_task1()
 	GPIOB->MODER &= ~(GPIO_MODER_MODE12);
 
 	uint8_t press_count = 0;
-	_Bool is_pressed = 0;
+	uint8_t is_pressed = 0;
 	//счётчик кадров анимации
 	uint8_t frame = 0;
 
 	//кадры анимации "победы"
-	_Bool victory_frames[][4] = { { 0, 0, 0, 1 },
-								  { 0, 0, 1, 0 },
-								  { 0, 1, 0, 0 },
-								  { 1, 0, 0, 0 },
-								  { 0, 1, 0, 0 },
-								  { 0, 0, 1, 0 } };
+	uint8_t victory_frames[] = { 0b0001,
+								 0b0010,
+								 0b0100,
+								 0b1000,
+								 0b0100,
+								 0b0010 };
 
 	//кадры анимации "ошибки"
-	_Bool error_frames[][4] = { { 0, 0, 0, 0 },
-								{ 1, 0, 0, 0 },
-								{ 1, 1, 0, 0 },
-								{ 1, 1, 1, 0 },
-								{ 1, 1, 1, 1 },
-								{ 0, 1, 1, 1 },
-								{ 0, 0, 1, 1 },
-								{ 0, 0, 0, 1 } };
+	uint8_t error_frames[] = { 0b0000,
+							   0b1000,
+							   0b1100,
+							   0b1110,
+							   0b1111,
+							   0b0111,
+							   0b0011,
+							   0b0001 };
 
 	//количество кадров анимации "победы"
 	uint8_t victory_num_of_frames = sizeof(victory_frames) / sizeof(victory_frames[0]);
@@ -175,6 +171,18 @@ void cr_task1()
 	while(1)
 	{
 		dumb_delay(100000);
+
+		if((GPIOB->IDR & GPIO_IDR_ID12) == 0 && is_pressed == 0)
+		{
+			is_pressed = 1;
+			//изменение значения счётчика нажатий с 0 на 1 или с 1 на 0
+			press_count ^= 1;
+			frame = 0;
+		}
+		else if(GPIOB->IDR & GPIO_IDR_ID12)
+		{
+			is_pressed = 0;
+		}
 
 		//очистка состояния загораемых светодиодов
 		GPIOE->ODR &= ~(GPIO_ODR_OD12
@@ -186,35 +194,23 @@ void cr_task1()
 		if(press_count == 0)
 		{
 			if(frame >= victory_num_of_frames)
+			{
 				frame = 0;
+			}
 
-			GPIOE->ODR |= victory_frames[frame][0] << GPIO_ODR_OD12_Pos
-						| victory_frames[frame][1] << GPIO_ODR_OD13_Pos
-						| victory_frames[frame][2] << GPIO_ODR_OD14_Pos
-						| victory_frames[frame][3] << GPIO_ODR_OD15_Pos;
+			GPIOE->ODR |= victory_frames[frame] << GPIO_ODR_OD12_Pos;
 		}
 		else
 		{
 			if(frame >= error_num_of_frames)
+			{
 				frame = 0;
+			}
 
-			GPIOE->ODR |= error_frames[frame][0] << GPIO_ODR_OD12_Pos
-						| error_frames[frame][1] << GPIO_ODR_OD13_Pos
-						| error_frames[frame][2] << GPIO_ODR_OD14_Pos
-						| error_frames[frame][3] << GPIO_ODR_OD15_Pos;
+			GPIOE->ODR |= error_frames[frame] << GPIO_ODR_OD12_Pos;
 		}
 
 		frame++;
-
-		if((GPIOB->IDR & GPIO_IDR_ID12) == 0 && is_pressed == 0)
-		{
-			is_pressed = 1;
-			//изменение значения счётчика нажатий с 0 на 1 или с 1 на 0
-			press_count ^= 1;
-			frame = 0;
-		}
-		else if(GPIOB->IDR & GPIO_IDR_ID12)
-			is_pressed = 0;
 	}
 }
 
@@ -241,24 +237,40 @@ void cr_task2()
 		dumb_delay(10000);
 
 		if((GPIOB->IDR & GPIO_IDR_ID12) == 0)
-			GPIOE->ODR |= 1 << GPIO_ODR_OD12_Pos;
+		{
+			GPIOE->BSRR = GPIO_BSRR_BS12;
+		}
 		else
-			GPIOE->ODR &= ~GPIO_ODR_OD12;
+		{
+			GPIOE->BSRR = GPIO_BSRR_BR12;
+		}
 
 		if((GPIOB->IDR & GPIO_IDR_ID13) == 0)
-			GPIOE->ODR |= 1 << GPIO_ODR_OD13_Pos;
+		{
+			GPIOE->BSRR = GPIO_BSRR_BS13;
+		}
 		else
-			GPIOE->ODR &= ~GPIO_ODR_OD13;
+		{
+			GPIOE->BSRR = GPIO_BSRR_BR13;
+		}
 
 		if((GPIOB->IDR & GPIO_IDR_ID14) == 0)
-			GPIOE->ODR |= 1 << GPIO_ODR_OD14_Pos;
+		{
+			GPIOE->BSRR = GPIO_BSRR_BS14;
+		}
 		else
-			GPIOE->ODR &= ~GPIO_ODR_OD14;
+		{
+			GPIOE->BSRR = GPIO_BSRR_BR14;
+		}
 
 		if((GPIOB->IDR & GPIO_IDR_ID15) == 0)
-			GPIOE->ODR |= 1 << GPIO_ODR_OD15_Pos;
+		{
+			GPIOE->BSRR = GPIO_BSRR_BS15;
+		}
 		else
-			GPIOE->ODR &= ~GPIO_ODR_OD15;
+		{
+			GPIOE->BSRR = GPIO_BSRR_BR15;
+		}
 	}
 }
 
@@ -280,56 +292,49 @@ void cr_task3()
 					| GPIO_MODER_MODE14);
 
 	uint8_t counter = 0,
-			buffer = 0,
 			b1_is_pressed = 0,
-			b2_is_pressed = 0,
-			b1, b2, b3, b4;
+			b2_is_pressed = 0;
 
 	while(1)
 	{
 		dumb_delay(10000);
 
-		GPIOE->ODR &= ~(GPIO_ODR_OD12
-					  | GPIO_ODR_OD13
-					  | GPIO_ODR_OD14
-					  | GPIO_ODR_OD15);
-
-		buffer = counter;
-		b1 = buffer % 2;
-		buffer = buffer >> 1;
-		b2 = buffer % 2;
-		buffer = buffer >> 1;
-		b3 = buffer % 2;
-		buffer = buffer >> 1;
-		b4 = buffer % 2;
-
-		GPIOE->ODR |= b1 << GPIO_ODR_OD12_Pos
-					| b2 << GPIO_ODR_OD13_Pos
-					| b3 << GPIO_ODR_OD14_Pos
-					| b4 << GPIO_ODR_OD15_Pos;
-
 		if((GPIOB->IDR & GPIO_IDR_ID12) == 0 && b1_is_pressed == 0)
 		{
 			b1_is_pressed = 1;
 			counter++;
-			if(counter >= 16)
-				counter = 0;
+			counter &= 0b1111;
 		}
 		else if(GPIOB->IDR & GPIO_IDR_ID12)
+		{
 			b1_is_pressed = 0;
+		}
 
 		if((GPIOB->IDR & GPIO_IDR_ID13) == 0 && b2_is_pressed == 0)
 		{
 			b2_is_pressed = 1;
 			counter--;
-			if(counter >= 16)
-				counter = 15;
+			counter &= 0b1111;
 		}
 		else if(GPIOB->IDR & GPIO_IDR_ID13)
+		{
 			b2_is_pressed = 0;
+		}
 
 		if((GPIOB->IDR & GPIO_IDR_ID14) == 0)
+		{
 			counter = 0;
+		}
+
+		if(((GPIOE->ODR >> GPIO_ODR_OD12_Pos) & 0b1111) ^ counter)
+		{
+			GPIOE->ODR &= ~(GPIO_ODR_OD12
+						  | GPIO_ODR_OD13
+						  | GPIO_ODR_OD14
+						  | GPIO_ODR_OD15);
+
+			GPIOE->BSRR = counter << GPIO_BSRR_BS12_Pos;
+		}
 	}
 
 }
@@ -350,71 +355,64 @@ void cr_task4()
 	GPIOB->MODER &= ~(GPIO_MODER_MODE12);
 
 	uint8_t counter = 0,
-			buffer = 0;
-	_Bool is_pressed = 0,
-		  b1, b2, b3, b4;
+			is_pressed = 0;
+
+	uint32_t long_press_delay = 500000;
 
 	while(1)
 	{
 		dumb_delay(100000);
 
-		GPIOE->ODR &= ~(GPIO_ODR_OD12
-					  | GPIO_ODR_OD13
-					  | GPIO_ODR_OD14
-					  | GPIO_ODR_OD15);
-
-		buffer = counter;
-		b1 = buffer % 2;
-		buffer = buffer >> 1;
-		b2 = buffer % 2;
-		buffer = buffer >> 1;
-		b3 = buffer % 2;
-		buffer = buffer >> 1;
-		b4 = buffer % 2;
-
-		GPIOE->ODR |= b1 << GPIO_ODR_OD12_Pos
-					| b2 << GPIO_ODR_OD13_Pos
-					| b3 << GPIO_ODR_OD14_Pos
-					| b4 << GPIO_ODR_OD15_Pos;
-
-		uint8_t i = 0;
+		uint32_t i = 0;
 
 		if((GPIOB->IDR & GPIO_IDR_ID12) == 0 && is_pressed == 0)
 		{
 			//for нужен, чтобы после короткого нажатия не нужно было ждать время как для длинного нажатия
-			for(; i < 50; i++)
+			for(; i < long_press_delay; i++)
 			{
-				dumb_delay(10000);
-
 				if(GPIOB->IDR & GPIO_IDR_ID12)
 				{
 					counter++;
-					if(counter >= 16)
-						counter = 0;
+					counter &= 0b1111;
 					break;
 				}
 			}
 
-			if (i == 50)
+			if (i == long_press_delay)
+			{
 				counter = 0;
+			}
 		}
 
 		//проверка, нажата ли кнопка после выполнения действия, чтобы с новой итерацией цикла while не происходили действия,
 		//пока кнопка не будет отпущена
 		if((GPIOB->IDR & GPIO_IDR_ID12) == 0)
+		{
 			is_pressed = 1;
+		}
 		else
+		{
 			is_pressed = 0;
+		}
+
+		if(((GPIOE->ODR >> GPIO_ODR_OD12_Pos) & 0b1111) ^ counter)
+		{
+			GPIOE->ODR &= ~(GPIO_ODR_OD12
+						  | GPIO_ODR_OD13
+						  | GPIO_ODR_OD14
+						  | GPIO_ODR_OD15);
+
+			GPIOE->BSRR = counter << GPIO_BSRR_BS12_Pos;
+		}
 	}
 }
 
 void cr_taskA()
 {
-	/*Я так до конца не разобрался где лучше использовать ODR, а где BSRR. В ODR можно напрямую менять состояния выводов,
-     * когда как в BSRR для установки и сброса нужно выставлять единицы в отдельные части регистра.
-	 * То, как я использовал ODR в творческом задании 1, очень похоже на то, как нужно работать с BSRR,
-	 * поэтому на примере данного задания большой разницы между ними я не заметил.
-	 * Разве что очищать состояния выводов удобнее и понятнее через BSRR. */
+	/* BSRR используют, когда требуется выставить определёные логические уровни на выходах,
+	 * вне зависимости от их прошлого состояния.
+	 * ORD же используют, когда нужно изменить значения на выходах из одного состояния в другое.
+	 * Из минусов - могут быть проблемы при одновременном использовании данного регистра в основной программе и в прерывании.*/
 	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOEEN | RCC_AHB2ENR_GPIOBEN;
 
 	GPIOE->MODER &= ~(GPIO_MODER_MODE12
@@ -429,27 +427,27 @@ void cr_taskA()
 	GPIOB->MODER &= ~(GPIO_MODER_MODE12);
 
 	uint8_t press_count = 0;
-	_Bool is_pressed = 0;
+	uint8_t is_pressed = 0;
 	//счётчик кадров анимации
 	uint8_t frame = 0;
 
 	//кадры анимации "победы"
-	_Bool victory_frames[][4] = { { 0, 0, 0, 1 },
-								  { 0, 0, 1, 0 },
-								  { 0, 1, 0, 0 },
-								  { 1, 0, 0, 0 },
-								  { 0, 1, 0, 0 },
-								  { 0, 0, 1, 0 } };
+	uint8_t victory_frames[] = { 0b0001,
+								 0b0010,
+								 0b0100,
+								 0b1000,
+								 0b0100,
+								 0b0010 };
 
 	//кадры анимации "ошибки"
-	_Bool error_frames[][4] = { { 0, 0, 0, 0 },
-								{ 1, 0, 0, 0 },
-								{ 1, 1, 0, 0 },
-								{ 1, 1, 1, 0 },
-								{ 1, 1, 1, 1 },
-								{ 0, 1, 1, 1 },
-								{ 0, 0, 1, 1 },
-								{ 0, 0, 0, 1 } };
+	uint8_t error_frames[] = { 0b0000,
+							   0b1000,
+							   0b1100,
+							   0b1110,
+							   0b1111,
+							   0b0111,
+							   0b0011,
+							   0b0001 };
 
 	//количество кадров анимации "победы"
 	uint8_t victory_num_of_frames = sizeof(victory_frames) / sizeof(victory_frames[0]);
@@ -460,36 +458,6 @@ void cr_taskA()
 	{
 		dumb_delay(100000);
 
-		//очистка состояния загораемых светодиодов
-		GPIOE->BSRR |= GPIO_BSRR_BR12
-					 | GPIO_BSRR_BR13
-					 | GPIO_BSRR_BR14
-					 | GPIO_BSRR_BR15;
-
-		//изменение анимации после нажатия кнопки
-		if(press_count == 0)
-		{
-			if(frame >= victory_num_of_frames)
-				frame = 0;
-
-			GPIOE->BSRR |= victory_frames[frame][0] << GPIO_BSRR_BS12_Pos
-						 | victory_frames[frame][1] << GPIO_BSRR_BS13_Pos
-						 | victory_frames[frame][2] << GPIO_BSRR_BS14_Pos
-						 | victory_frames[frame][3] << GPIO_BSRR_BS15_Pos;
-		}
-		else
-		{
-			if(frame >= error_num_of_frames)
-				frame = 0;
-
-			GPIOE->BSRR |= error_frames[frame][0] << GPIO_BSRR_BS12_Pos
-						 | error_frames[frame][1] << GPIO_BSRR_BS13_Pos
-						 | error_frames[frame][2] << GPIO_BSRR_BS14_Pos
-						 | error_frames[frame][3] << GPIO_BSRR_BS15_Pos;
-		}
-
-		frame++;
-
 		if((GPIOB->IDR & GPIO_IDR_ID12) == 0 && is_pressed == 0)
 		{
 			is_pressed = 1;
@@ -498,7 +466,37 @@ void cr_taskA()
 			frame = 0;
 		}
 		else if(GPIOB->IDR & GPIO_IDR_ID12)
+		{
 			is_pressed = 0;
+		}
+
+		//очистка состояния загораемых светодиодов
+		GPIOE->BSRR = GPIO_BSRR_BR12
+					| GPIO_BSRR_BR13
+					| GPIO_BSRR_BR14
+					| GPIO_BSRR_BR15;
+
+		//изменение анимации после нажатия кнопки
+		if(press_count == 0)
+		{
+			if(frame >= victory_num_of_frames)
+			{
+				frame = 0;
+			}
+
+			GPIOE->BSRR = victory_frames[frame] << GPIO_BSRR_BS12_Pos;
+		}
+		else
+		{
+			if(frame >= error_num_of_frames)
+			{
+				frame = 0;
+			}
+
+			GPIOE->BSRR = error_frames[frame] << GPIO_BSRR_BS12_Pos;
+		}
+
+		frame++;
 	}
 }
 
